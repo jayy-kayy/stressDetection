23d33b711e6851bfe89d553d535b57695898f544
jayy-kayy <shashankjk4@gmail.com>
2024-03-01 10:29:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const first=document.getElementById("1");
+first.addEventListener('click',()=>{
+    window.location.href='card1.html';
+});
+
+const second=document.getElementById("2");
+first.addEventListener('click',()=>{
+    window.location.href='card2.html';
+});
+
+const third=document.getElementById("3");
+first.addEventListener('click',()=>{
+    window.location.href='card3.html';
+});
+
+const fourth=document.getElementById("4");
+first.addEventListener('click',()=>{
+    window.location.href='card4.html';
+});
